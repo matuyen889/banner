1ebad3a69e35b6717bdde225d78080cdc7c03530
matuyen <86024390+matuyen889@users.noreply.github.com>
2024-10-16 03:53:31 +0700

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,6 +1,6 @@
 GREEN = "\33[94m"
 RESET = "\033[0m"
 banner = f"""{BLUE}                                                       
-From:MATUYEN.ETH
-
+From:     MATUYEN.ETH
+Cảm ơn vì đã dùng
 print(banner)
